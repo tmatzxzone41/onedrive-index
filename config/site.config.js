5c05293e6a7365272cc25d9502efa1ef1e955892
tmatzxzone41 <105190138+tmatzxzone41@users.noreply.github.com>
2024-03-21 08:45:06 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,10 +17,10 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "TmatzXzonEv Vault",
+  title: "Yep Cock",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: 'Public',
+  baseDirectory: 'Private',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -43,11 +43,8 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
-  "/Apps",
-   "/aria2",
-    "/Attachment",
-     "/qBittorrent",
-      "/Telegram Upload",
+  "/Transfered",
+   "/Hani"
 ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
